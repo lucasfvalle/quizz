2a7b94c408940a8a2d51c382c2687958bd3b72b4
Lucas Valle <lucasfvalle@gmail.com>
2022-07-19 13:01:44 -0300

    Design da primeira tela/seleção de tema.

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+// Questões
+var prog_question = [
+    {
+        "pergunta" : "Quem foi o criador do Javascript?",
+        "resposta": "Brendan Eich", 
+        "a" : "Brendan Eich",
+        "b" : "Dennis Ritchie",
+        "c" : "James Gosling",
+    },
+    {
+        "pergunta" : "Quem foi o criador da Microsoft",
+        "resposta": "Bill Gates", 
+        "a" : "Steve Jobs",
+        "b" : "Linus Torvalds",
+        "c" : "Bill Gates",
+    },
+    {
+        "pergunta" : "Quem criou o Linux?",
+        "resposta": "Linus Torvalds", 
+        "a" : "Linus Torvald",
+        "b" : "Steve Wozniak",
+        "c" : "Alan Turing",
+    },
+]
+var animal_question = [
+    {
+        "pergunta" : "Quantos meses dura a gestação da baleia?",
+        "resposta" : "16 meses",
+        "a": "9 meses",
+        "b": "16 meses",
+        "c": "36 meses",
+    },
+    {
+        "pergunta" : "Qual o maior animal do mundo?",
+        "resposta" : "Baleia azul antártica",
+        "a": "Elefante africano",
+        "b": "Girafa",
+        "c": "Baleia azul antártica",
+    },
+    {
+        "pergunta" : "Qual a maior raça de cachorro?",
+        "resposta" : "Mastim Inglês",
+        "a": "Mastim Inglês",
+        "b": "Golden Retriever",
+        "c": "Pastor alemão",
+    },
+
+]
+
+//let randomnumb = Math.floor(Math.random() * 10);
+var ani = animal_question[0];
+console.log(ani.pergunta);
+console.log(ani.resposta);
